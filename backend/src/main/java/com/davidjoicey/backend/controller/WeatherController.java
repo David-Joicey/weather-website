d3b07981e0d8f770386fe763abc91b241d4f1de0
david <c5021154@newcastle.ac.uk>
2026-08-12 19:01:16 +0100

    Added weather forecast and data model to /weather/city endpoint

--- backend/src/main/java/com/davidjoicey/backend/controller/WeatherController.java
+++ backend/src/main/java/com/davidjoicey/backend/controller/WeatherController.java
@@ -1,23 +1,33 @@
 package com.davidjoicey.backend.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.davidjoicey.backend.model.WeatherData;
+import com.davidjoicey.backend.model.WeatherForecast;
 
 import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
-
-    @PostConstruct
-    public void init() {
-        System.out.println("WeatherController LOADED");
-    }
-
     @GetMapping("/{city}")
-    public String getWeather(@PathVariable String city) {
-        return "Weather for " + city;
+    public WeatherForecast getWeather(@PathVariable String city) {
+
+        List<WeatherData> daily = List.of(
+            new WeatherData(15.7, 50.2, 1.3, "cloudy"),
+            new WeatherData(20.1, 70.6, 5.0, "Sunny")
+        );
+
+        return new WeatherForecast(
+            city,
+            53.8,
+            -1.55,
+            daily
+        );
     }
 }
--- backend/src/main/java/com/davidjoicey/backend/model/WeatherData.java
+++ backend/src/main/java/com/davidjoicey/backend/model/WeatherData.java
@@ -1,7 +1,6 @@
 package com.davidjoicey.backend.model;
 
 public record WeatherData (
-    String city,
     double temperature,
     double humidity,
     double precipitation,
