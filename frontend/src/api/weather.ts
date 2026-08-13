2c95da20ae49403f844d50c1217dd9470ebdfd62
david <c5021154@newcastle.ac.uk>
2026-08-13 23:05:41 +0100

    Added API call to backend in Search.tsx, displays JSON on page of weather data if found (temporary)

--- frontend/src/api/weather.ts
+++ frontend/src/api/weather.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getWeather = async (city: string) => {
+export const getWeather = async (location: string) => {
   const response = await axios.get(
-    `http://localhost:8080/api/weather/${city}`
+    `http://localhost:8080/weather/${location}`
   );
   return response.data;
 };
--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,18 +1,52 @@
 import "./Search.css";
+import { getWeather } from "../api/weather";
+import { useState } from "react";
 
 function search() {
+    const [location, setLocation] = useState("");
+
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const [weather, setWeather] = useState<any>(null);
+
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+        if (!location.trim()) return;
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const data = await getWeather(location);
+            console.log("Weather data:", data);
+            setWeather(data);
+        } catch (err) {
+            setError("Could not fetch weather data");
+        } finally {
+            setLoading(false);
+        }
+  }
+
     return(
         <div className = "page-container">
             <h1 className = "title">Weather Search</h1>
 
-            <div className = "search-container">
+            <form className = "search-container" onSubmit={handleSubmit}>
                 <input
                 className = "search-bar"
                 type = "text"
                 placeholder = "Location..."
+                value = {location}
+                onChange={(e) => setLocation(e.target.value)}
                 />
-            </div>
 
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+            {weather && <pre>{JSON.stringify(weather, null, 2)}</pre>}
         </div>
     )
 }
